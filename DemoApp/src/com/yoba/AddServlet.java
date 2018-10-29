67f50cce4d98ed037e442c327bb437a78e30e61b
yobastono <weissjoscha1@gmail.com>
2018-10-29 23:12:36 +0100

    index.html has some problem

--- DemoApp/src/com/yoba/AddServlet.java
+++ DemoApp/src/com/yoba/AddServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
