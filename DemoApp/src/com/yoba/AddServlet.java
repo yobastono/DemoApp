129ccaa2a6cc8d834d98140be48f496858613ede
yobastono <weissjoscha1@gmail.com>
2018-10-30 12:58:36 +0100

    problems with index ?

--- DemoApp/src/com/yoba/AddServlet.java
+++ DemoApp/src/com/yoba/AddServlet.java
@@ -9,7 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		int k = i + j;
+//		
+//		PrintWriter out = res.getWriter();
+//		out.println(k);
+//	}
+//	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
@@ -17,4 +26,13 @@ public class AddServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 		out.println(k);
 	}
+	
+//	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		int k = i + j;
+//		
+//		PrintWriter out = res.getWriter();
+//		out.println(k);
+//	}
 }
